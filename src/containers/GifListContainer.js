6fc964aed829314bf41be87c3f72b2e606cf7c89
blessed-installation-48cc <sima.jazaa@gmail.com>
2021-03-27 08:54:04 +0000

    Automatically backed up by Learn

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react'
-import GifList from '../components/GifList'
 import GifSearch from '../components/GifSearch'
+import GifList from '../components/GifList'
 
 class GifListContainer extends Component {
-
   state = {
     gifs: []
   }
 
   render() {
     return(
+      
       <div>
         <GifSearch fetchGIFs={this.fetchGIFs} />
         <GifList gifs={this.state.gifs} />
       </div>
+      
     )
   }
 
