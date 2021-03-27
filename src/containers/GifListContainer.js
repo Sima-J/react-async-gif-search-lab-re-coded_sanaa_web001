dd325e3c86a0f7e8437b24ada3d92649edce5cfb
blessed-installation-48cc <sima.jazaa@gmail.com>
2021-03-27 08:52:25 +0000

    Automatically backed up by Learn

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -1,27 +1,33 @@
+import React, { Component } from 'react'
 import GifList from '../components/GifList'
 import GifSearch from '../components/GifSearch'
-export default class GifListContainer extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      gifs: []
-    }
+
+class GifListContainer extends Component {
+
+  state = {
+    gifs: []
   }
-  handleFetch = (query) =>{
-    fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g`)
-    .then(res => res.json())
-    .then(data => {
-      this.setState({
-        gifs: data.data
-      })
-    })
-  }
+
   render() {
-    return (
+    return(
       <div>
-        <GifSearch handleSearch={this.handleFetch}/>
-        {this.state.gifs.map((gif, i)=> (<GifList key={i} gif={gif.images.original.url}/>))}
+        <GifSearch fetchGIFs={this.fetchGIFs} />
+        <GifList gifs={this.state.gifs} />
       </div>
     )
   }
-} 
+
+  fetchGIFs = (query) => {
+    fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g&limit=3`)
+      .then(res => res.json())
+      .then(({data}) => {
+        this.setState({ gifs: data.map( gif => ({ url: gif.images.original.url }) ) })
+      })
+  }
+
+  componentDidMount() {
+    this.fetchGIFs()
+  }
+}
+
+export default GifListContainer
